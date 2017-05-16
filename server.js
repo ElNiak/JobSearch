9dc1eb770f065ef39896c1a160b27be3d349c76b
Mihien <simon_hick@hotmail.com>
2017-05-16 18:08:26 +0200

    petits changements - je sais pas ce que je fais

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 var app = express();
 
+app.use(express.bodyParser());
 app.use(express.static('views'));
-
 app.set('view engine', 'ejs');
 
 //page d'accueil
@@ -15,6 +20,20 @@ app.get('/', function(req, res) {
 //register
 app.get('/register', function(req, res) {
 	res.render(__dirname + "/views/"+'register.ejs');
+});
+
+app.post('/register_post',urlencodedParser, function(req, res){
+  response = {
+      first_name:req.body.firstName,
+      last_name:req.body.lastName,
+      email:req.body.email,
+      password:req.body.password,
+      birthDate:req.body.birthDate,
+      country:req.body.country,
+      gender:req.body.gender
+  }
+   console.log(response);
+   res.end(JSON.stringify(response));
 });
 
 //page CV
